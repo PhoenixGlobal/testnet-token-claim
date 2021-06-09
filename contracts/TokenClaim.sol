490ec9912aa8da581e674fcb2bb71b97f5419d69
Conight <xfddwhh@gmail.com>
2021-06-09 11:09:46 +0800

    feat(claim): add claim PHB and can custom claim amount

--- contracts/TokenClaim.sol
+++ contracts/TokenClaim.sol
@@ -2,24 +2,32 @@
 pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./Owned.sol";
 
-contract TokenClaim {
-    IERC20 public token;
+contract TokenClaim is Owned {
+    IERC20 public tokenHZN;
+    IERC20 public tokenPHB;
     address public spender;
     uint256 public minClaimPeriod;
-    uint256 public transferAmount;
 
     constructor(
-        address _token,
+        address _tokenHZN,
+        address _tokenPHB,
         address _spender,
-        uint256 _transferAmount
-    ) {
-        token = IERC20(_token);
+        address _owner
+    ) Owned(_owner) {
+        tokenHZN = IERC20(_tokenHZN);
+        tokenPHB = IERC20(_tokenPHB);
         spender = _spender;
-        transferAmount = _transferAmount;
     }
 
-    function claim() public {
-        token.transferFrom(spender, msg.sender, transferAmount);
+    function claimHZN(uint256 _amount) external {
+        require(_amount <= 1000 * 1e18, "Transfer amount too large.");
+        tokenHZN.transferFrom(spender, msg.sender, _amount);
+    }
+
+    function claimPHB(uint256 _amount) external {
+        require(_amount <= 10000000 * 1e18, "Transfer amount too large.");
+        tokenPHB.transferFrom(spender, msg.sender, _amount);
     }
 }
