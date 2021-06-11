a1b52566c7e1e9c6b79c274eb305cdd2834d660f
Conight <xfddwhh@gmail.com>
2021-06-11 09:52:22 +0800

    feat(phb): add phb claim
    '

--- contracts/TokenClaim.sol
+++ contracts/TokenClaim.sol
@@ -5,20 +5,20 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./Owned.sol";
 
 contract TokenClaim is Owned {
-    IERC20 public tokenHZN;
+    IERC20 public tokenPHB;
     address public spender;
 
     constructor(
-        address _tokenHZN,
+        address _tokenPHB,
         address _spender,
         address _owner
     ) Owned(_owner) {
-        tokenHZN = IERC20(_tokenHZN);
+        tokenPHB = IERC20(_tokenPHB);
         spender = _spender;
     }
 
-    function claimHZN(uint256 _amount) external {
-        require(_amount <= 1000 * 1e18, "Transfer amount too large.");
-        tokenHZN.transferFrom(spender, msg.sender, _amount);
+    function claimPHB(uint256 _amount) external {
+        require(_amount <= 1000000 * 1e18, "Transfer amount too large.");
+        tokenPHB.transferFrom(spender, msg.sender, _amount);
     }
 }
